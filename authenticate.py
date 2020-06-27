ade5171bb5019ca4b687a3251006cd62028480ff
DanielEbert <53190379+DanielEbert@users.noreply.github.com>
2020-06-27 16:02:08 +0200

    Cleanup

--- authenticate.py
+++ authenticate.py
@@ -1,8 +1,5 @@
 import jwt
 
-SECRET_KEY = "7ba933c6a65cc9f786e7d50b7aa97ad6b5a4220d77880a2db974bf49d7ed1554"
-
-
 def authenticate_user_with_token(access_token: str) -> str:
     try:
         token = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
